64b33a84ae95c063f94ba34c5224ebeeab1c6dce
Filipe <lipecavalcante17@hotmail.com>
2022-09-08 01:16:59 -0300

    Inserção do modelo de elencos no painel administrador

--- cesmaFlix/filmes/admin.py
+++ cesmaFlix/filmes/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from filmes.models.genero import Genero
 from filmes.models.diretor import Diretor
+from filmes.models.elenco import Elenco
 
 admin.site.register(Genero)
 admin.site.register(Diretor)
+admin.site.register(Elenco)
